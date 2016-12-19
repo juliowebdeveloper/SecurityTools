3410cd461cbbbe2cf979fc3669b5e84d7454a1e3
Julio <juliowebdeveloper@hotmail.com>
2016-12-19 09:25:41 -0200

    Adicionando novas functions utilziando filter, map, flatmap e foreach

--- src/main/kotlin/Program.kt
+++ src/main/kotlin/Program.kt
@@ -1,4 +1,4 @@
-import com.rsk.Providers
+ import com.rsk.Providers
 
 /**
  * Created by Teste2 on 16/12/2016.
@@ -6,12 +6,79 @@ import com.rsk.Providers
 
 
 fun main(args: Array<String>){
+    val providers = Providers()
+    val details = providers.getAllProviders() //Trazendo uma lista de providerDetails a partir do dataclass
+    details.forEach { detail -> println("${detail.providerName} ------ ${detail.name}")}
+                        //fazendo o foreach nessa lista de provider details e para cada detail, é exibido suas propriedades
 
-    listProvidersNoInstance()
+    details.forEach{ println("${it.providerName} --- ${it.name}")} //Omitindo o nome da variavel detail - deixa o codigo mais limpo
+
+    //Já que o ProviderDetails é uma dataClass ele já possui tostring ou seja não precisa printar todos os atributos
+    println("_-__-__--_-_-----_--_-------________---")
+    details.forEach { println(it) }
+
+    //Nesse caso por estarmos passando um unico parametro para a function, podemos mudar para referencia à function
+    details.forEach (::println)
+
+
+    val details2 = providers.getAllProvidersRandomEX("Random")//queremos uma funcao que gere numeros random
+    println("-----------------------------Filter--------------------------------")
+
+    details2.forEach ( ::println )
+
+
+
+
+    /*
+    //listProvidersNoInstance()
+        getAllProviders{
+            key, value -> println("\t--------$key:         $value")
+        }//Ao inves de passar como um parametros para a function, utilizamos {}
+                     //{} é um indicador que o que estamos passando é uma function
+              //então nesse ponto precisamos definir uma function que recebe duas strings e retorna nada*/
 }
+//key, value = nome das variaveis strings
+//-> go to especificamos o que faremos com elas, no caso o mesmo que o println
+//Isso é a passada de High Ordered Functions
+//Com isso estamos passando uma strategy que será executada
 
 
 
+//Map: a idéia é mapear no sentido de, passamos uma lista de nomes e ele mapeia numa lista de usuários, collection de uma coisa para uma collection que outra
+
+
+
+
+
+
+
+//fn = function type
+//queremos replicar o que passamos para o foreach - passamos 2 strings e retorna nada (void)
+fun getAllProviders(fn: (String, String) -> Unit) {
+    val allProviders = Providers.getProviders()//static
+    val it = allProviders.iterator()
+    while (it.hasNext()) {
+        val provider = it.next()
+        println(provider.name)
+        //Para cada provider, é exibido uma key, value , utilizando um formato lambda no foreach
+      /*  provider.forEach { key, value ->
+            println("\t$key: $value")   //Foreach recebe uma function
+            //fun callback(va1:String, val2:String):Unit - recebe duas strings e retorna nada
+        }*/
+        
+        //Ao inves de chamar o println, chamaremos a function que recebemos como parametro(Na verdade definida na chamada desse metodo)
+        //os toString são para que nao sejam tratados como objeto
+        //Quando chamamos o forEach nos temos o key, value que será passado para a chamada do foreach
+        //E nós chamamos o fn, que é a função passada ao chamar o getAllProviders e essa function irá printar os valores
+        provider.forEach { key, value ->
+            fn(key.toString(), value.toString())
+        }
+
+
+
+
+    }
+}
 
 fun  listProvidersInstance(){
     val providers = Providers()
--- src/main/kotlin/com/rsk/Providers.kt
+++ src/main/kotlin/com/rsk/Providers.kt
@@ -6,6 +6,12 @@ import java.security.Security
 /**
  * Created by Teste2 on 16/12/2016.
  */
+
+
+//Data class - Helper class que terá a informação de todos os providers, name é o hashimplementation
+data class ProviderDetails(val providerName: String, val name: String)
+
+
 class Providers {
 
     fun getProviders() : List<Provider>{
@@ -18,6 +24,80 @@ class Providers {
         return listOfProviders
     }
 
+    fun  getAllProvidersRandom(filter: String): List<ProviderDetails> {
+         //Filter é outra function dentro da collections library que pega uma High ordered Function
+        val providers = Security.getProviders()
+        val listOfProviders = mutableListOf<ProviderDetails>()
+        //Iremos filtrar a informação que nao precisamos e ai sim mapear essa informação
+        providers.forEach { provider ->
+            val providerDetails = provider.entries.filter { it ->  it.key.toString().contains(filter, true) } //ver se a key contain a filter string, se contiver retornamos de nao, não, o true é para ser caseSensitive
+                    .map {
+                ProviderDetails(provider.name, it.key.toString())
+            }
+            listOfProviders += providerDetails
+        } //Fazendo comparação com SQL filter é Where e Map é Select
+
+        return listOfProviders
+    }
+
+    fun  getAllProvidersRandomEX(filter: String): List<ProviderDetails> {
+        val providers = Security.getProviders()
+
+        //O problema dessa mudança é que forEach nao retorna nada, mas Map sim
+
+        return providers.flatMap { provider ->
+             provider.entries.filter { it ->  it.key.toString().contains(filter, true) } //ver se a key contain a filter string, se contiver retornamos de nao, não, o true é para ser caseSensitive
+                    .map {
+                        ProviderDetails(provider.name, it.key.toString())
+                    }
+        }
+        //Mapeando junto duas collections, mergeando elas e retornando a lista resultante
+        /*Como o map gera uma collection de collections quando passamos 2 collections, ex:
+        Collection 1[1,2,3]
+                Collection2[a,b]
+
+                 Ele irá gerar
+
+             Collection de collection  [ [1a,1b][2a,2b][3a,3b] ]
+                Para resolver isso utilizamos flatmap
+
+                Ele irá gerar :
+                                [1a, 1b, 2a,2b, 3a,3b] = Single Collection
+
+        */
+
+    }
+
+
+    fun getAllProviders(): List<ProviderDetails>{
+        val providers = Security.getProviders()
+        val listOfProviders= mutableListOf<ProviderDetails>() //Com isso irá criar uma lista mutavel listOfProviders do tipo ProviderDetails
+
+        //iterar pelos providers mapeando eles nos providerdetails
+        //para cada um desses providers eu quero criar um provider details
+        providers.forEach { provider->
+            val providerDetails = provider.entries.map {
+                //entry->ProviderDetails(provider.name, entry.key.toString())
+
+                //Não há a necessidade de dar um nome à variavel entry, kotlin ja o nomeia como "it", só precisa colcoar o comportamento
+                ProviderDetails(provider.name, it.key.toString())
+
+            } //Queremos mapear as "entries"
+              //Para cada entry queremos criar um ProviderDetails (data class com os valores providerName e name(hash))
+            //para conseguir esses valores, o providername vem de provider.name e o hash daquela entry.key
+            //com isso conseguimos um unico ProviderDetail object
+            listOfProviders += providerDetails //adicionando à lista com mecanismo de overload
+            //Com isso é feito uma filtragem naquela val providers e atraves do map, nos retorna uma nova lista de provider details ao terminar o foreach
+            //Em resumo: recebemos a lista de providers, mapeamos para uma lista de ProviderDetails e retornamos para o caller
+
+
+        }
+
+
+
+
+        return listOfProviders
+    }
 
     //por nao ter metodos static, utilizamos companion objects - singleton object
     companion object {
@@ -27,5 +107,7 @@ class Providers {
             return listOfProviders
         }
     }
+
+
 
 }
